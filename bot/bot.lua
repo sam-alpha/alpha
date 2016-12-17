9bf2d72e8caebae300c0c539c5fa33f168ab08a5
sam-alpha <mohamadsampayer@gmail.com>
2016-12-17 18:33:36 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {293729444},
     admin_users = {},
     disabled_channels = {}
   }
